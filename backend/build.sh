0faf066d60b85e55d083b022619882659c5e381a
AaronGodoy <36670741+AaronGodoy@users.noreply.github.com>
2026-06-03 16:45:49 -0400

    Disable database reset script to protect production data

--- backend/build.sh
+++ backend/build.sh
@@ -6,10 +6,13 @@ echo "Installing dependencies..."
 pip install -r requirements.txt
 
 echo "Running database initialization..."
-# Forzamos que se eliminen y creen todas las tablas para solucionar inconsistencias de migraciones en producción
-python reset_db.py
+# Ya no forzamos el reseteo porque estamos en etapa de produccion.
+# python reset_db.py
 
-# Marcar las migraciones como actualizadas para que alembic no intente correrlas
-alembic stamp head
+echo "Running migrations..."
+alembic upgrade head
+
+# Marcar las migraciones como actualizadas para que alembic no intente correrlas si no hay nuevas
+# alembic stamp head
 
 echo "Build script completed."
